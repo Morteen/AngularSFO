e3c452131d244e07ef3ea8904aff03fbe9a9f547
morteen <mortenolsen4@gmail.com>
2017-12-27 10:18:14 +0100

    Ryddet opp i registrert dene

--- SFO/src/app/reg-elev/reg-elev.component.ts
+++ SFO/src/app/reg-elev/reg-elev.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
 import { EleverService } from '../elever.service';
+import { Elev } from '../MyInterface/Elev';
 
 @Component({
   selector: 'app-reg-elev',
@@ -17,24 +18,9 @@ export class RegElevComponent implements OnInit {
       
     
     }
-  addElev(fname,ename,trinn,klasse,konTlf,info){
-    console.log(fname+""+ename+""+trinn+""+klasse+""+konTlf+""+info);
-    this.elev={fname:fname,
-    ename:ename,
-  trinn:trinn,
-  klasse:klasse,
-  kontaktTlf:konTlf,
-  info:info
-  }
+ 
 
-
-  this.elever.push(this.elev);
-  console.log(this.elever.length);
-  alert(this.elev.fname);
-    return false;
-  }
-
-test(fname: string, ename: string, tlf: string, info: string, trinn: number, klasse: string){
+  addElev(fname: string, ename: string, tlf: string, info: string, trinn: number, klasse: string){
   this.elevService.postEnElev(fname,ename, tlf, info, trinn, klasse);
 }
 
@@ -43,12 +29,3 @@ test(fname: string, ename: string, tlf: string, info: string, trinn: number, kla
   
   }
   
-  
-  interface Elev{
-    fname:string,
-    ename:string;
-    trinn:number,
-    klasse:string,
-    kontaktTlf:number,
-    info:string
-  }
